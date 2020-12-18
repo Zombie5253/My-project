059d9cc21cab8273e172e625c18753803ea970ce
Burhanuddin <burhanuddinelectricwala78653@gmail.com>
2020-12-18 22:36:46 +0530

    commit

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -131,4 +131,4 @@ import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
